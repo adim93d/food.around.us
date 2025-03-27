108c1e2b2756b73b024764a689ff9f963491f540
Element <adim93d@gmail.com>
2025-03-27 11:51:24 +0200

    Bug Identified - File upload error, Anotated needed to be added

--- app/api/routers/identify_routes.py
+++ app/api/routers/identify_routes.py
@@ -24,11 +24,15 @@ async def identify_plant(
     files = []
     for image in images:
         contents = await image.read()
+        print(contents)
+        print(image.filename)
+        print(image.content_type)
         files.append(('images', (image.filename, contents, image.content_type)))
 
     data = [('organs', organ) for organ in organs]
 
     try:
+        print("Starting first API call to Planetnet")
         response = requests.post(API_ENDPOINT, files=files, data=data)
 
         if response.status_code != 200:
@@ -37,6 +41,7 @@ async def identify_plant(
             raise HTTPException(status_code=response.status_code, detail=error_detail)
 
         result = response.json()
+        print(f"Finished first API Call: {result}")
         print(result)
         if not result.get('results'):
             raise HTTPException(status_code=404, detail="No identification results found.")
@@ -44,10 +49,12 @@ async def identify_plant(
         top_result = result['results'][0]
         species_name = top_result['species']['scientificNameWithoutAuthor']
         family_name = top_result['species']['family']['scientificNameWithoutAuthor']
+        print("Starting 2 API code to OpenAI")
         plant_info = get_detailed_plant_info(species_name)
         is_edible = plant_info.get("edible")
         edible_parts = plant_info.get("edible_parts")
         safety = plant_info.get("safety")
+        print(f"Finsished 2 API call: result: {plant_info}")
 
         return {"species_name": species_name, "family_name": family_name, "is_edible": is_edible, "edible_parts": edible_parts, "safety": safety}
 
