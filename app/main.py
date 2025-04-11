93d0efaf53e67ca0ad1aee61ef6c7b35c5fbdc26
Element <adim93d@gmail.com>
2025-04-11 21:56:56 +0300

    Added logging to find the problem in deployment

--- app/main.py
+++ app/main.py
@@ -1,51 +1,49 @@
-# main.py
 from fastapi import FastAPI
 from dotenv import load_dotenv
+import logging
+from contextlib import asynccontextmanager
 
-from app.database.database import engine
-from app.database.database import Base
-from app.api.routers import plant_routes, user_routes, user_plants_routes, auth, recipe_routes, identify_routes, scan_routes
-
+from app.database.database import engine, Base
+from app.api.routers import (
+    plant_routes, 
+    user_routes, 
+    user_plants_routes, 
+    auth, 
+    recipe_routes, 
+    identify_routes, 
+    scan_routes
+)
 
 load_dotenv()
 
+# WARNING: Remove or conditionally execute drop_all in production environments.
 Base.metadata.drop_all(bind=engine)
 # Create tables if they do not exist
 Base.metadata.create_all(bind=engine)
 
 tags_metadata = [
-    {
-        "name": "Users",
-        "description": "CRUD Operations related to users."
-    },
-    {
-        "name": "Identify",
-        "description": "Operations related to identifying plants."
-    },
-    {
-        "name": "Plants",
-        "description": "CRUD Operations related to plants."
-    },
-    {
-        "name": "User-Plants",
-        "description": "CRUD Operations related to User-Plants relationship."
-    },
-    {
-        "name": "Recipes",
-        "description": "CRUD Operations related to recipes."
-    },
-    {
-        "name": "Auth",
-        "description": "Auth related Operations"
-    }
+    {"name": "Users", "description": "CRUD Operations related to users."},
+    {"name": "Identify", "description": "Operations related to identifying plants."},
+    {"name": "Plants", "description": "CRUD Operations related to plants."},
+    {"name": "User-Plants", "description": "CRUD Operations related to User-Plants relationship."},
+    {"name": "Recipes", "description": "CRUD Operations related to recipes."},
+    {"name": "Auth", "description": "Auth related Operations"}
 ]
 
-app = FastAPI(openapi_tags=tags_metadata)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("FastAPI is starting up...")
+    yield
+    logger.info("FastAPI is shutting down...")
+
+app = FastAPI(openapi_tags=tags_metadata, lifespan=lifespan)
 
 @app.get("/")
 def healthcheck():
-    return f"FastAPI is up and running!"
+    return "FastAPI is up and running!"
 
 # API routes
 app.include_router(identify_routes.router, prefix="/identify", tags=["Identify"])
